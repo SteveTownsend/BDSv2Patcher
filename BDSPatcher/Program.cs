442aa9a514cee3bc35eea2a7accd1c2f5aa86d28
Justin Swanson <justin.c.swanson@gmail.com>
2021-03-29 19:27:46 -0500

    Implicit listings went immutable
    Was improperly exposed as a modifiable collection.  Was made readonly, as intended, which means you have to make your own collection if you want to add extra things to it

--- BDSPatcher/Program.cs
+++ BDSPatcher/Program.cs
@@ -113,7 +113,7 @@ namespace BDSPatcher
                 throw new ArgumentException("Unable to get Better Dynamic Snow.esp plugin");
             }
             var materialMapping = MaterialMapping(bdsMod.Mod);
-            var skipMods = Implicits.Get(state.PatchMod.GameRelease).Listings;
+            var skipMods = Implicits.Get(state.PatchMod.GameRelease).Listings.ToHashSet();
             skipMods.Add(USSEPModKey);
 
             Console.WriteLine("{0} STAT", state.LoadOrder.PriorityOrder.WinningOverrides<IStaticGetter>().Count<IStaticGetter>());
